f7948a680d6ebf6250277efeaf6c670125582e39
Linus Unnebäck <linus@folkdatorn.se>
2018-11-20 16:27:59 +0000

    🎉 Export all TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,32 @@
-declare type MessageId = string
-declare type MessageBody = string
+declare namespace createClient {
+  type MessageId = string
+  type MessageBody = string
 
-declare type MimeType = 'text/plain' | 'text/html'
-declare type EmailAddress = { name?: string, address: string }
+  type MimeType = 'text/plain' | 'text/html'
+  type EmailAddress = { name?: string, address: string }
 
-interface MessageMeta {
-  date: string
-  from: EmailAddress | null
-  subject: string | null
+  interface MessageMeta {
+    date: string
+    from: EmailAddress | null
+    subject: string | null
+  }
+
+  interface OutgoingMessage {
+    from: EmailAddress
+    to: EmailAddress
+    subject: string
+    body: string
+  }
+
+  interface Client {
+    archiveMessage (id: MessageId): Promise<void>
+    fetchMessageBody (id: MessageId, withMimeType: MimeType): Promise<MessageBody>
+    fetchMessageMeta (id: MessageId): Promise<MessageMeta>
+    search (query: string): Promise<MessageId[]>
+    send (email: OutgoingMessage): Promise<void>
+  }
 }
 
-interface OutgoingMessage {
-  from: EmailAddress
-  to: EmailAddress
-  subject: string
-  body: string
-}
-
-interface Client {
-  archiveMessage (id: MessageId): Promise<void>
-  fetchMessageBody (id: MessageId, withMimeType: MimeType): Promise<MessageBody>
-  fetchMessageMeta (id: MessageId): Promise<MessageMeta>
-  search (query: string): Promise<MessageId[]>
-  send (email: OutgoingMessage): Promise<void>
-}
-
-declare function createClient (provider: 'gmail' | 'outlook', accessToken: string): Client
+declare function createClient (provider: 'gmail' | 'outlook', accessToken: string): createClient.Client
 
 export = createClient
